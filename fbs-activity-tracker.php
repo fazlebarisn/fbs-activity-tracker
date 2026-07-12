8a54657dd202e066aa6d77067234e206918901eb
fazlebarisn <fazlebarisn@gmail.com>
2026-07-12 14:03:50 +0600

    update

--- fbs-activity-tracker.php
+++ fbs-activity-tracker.php
@@ -9,7 +9,7 @@
  * Text Domain: fbs-activity-tracker
  * Domain Path: /languages
  * Requires at least: 5.0
- * Tested up to: 6.9
+ * Tested up to: 7.0
  * Requires PHP: 7.4
  * License: GPL v2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
